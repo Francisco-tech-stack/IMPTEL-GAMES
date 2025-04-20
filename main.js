7c90272f6050ae034b3074272194f13c374877a8
Francisco-tech-stack <franciscofrederico141@gmail.com>
2025-04-20 01:59:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ if(nome && sobr && nivel && curso){
   cd1.href = "perfil.html"
   cd1.textContent = nome+" "+sobr+"... Ver minha conta."
   cd1.style.color = 'blue'
-  cd1.classList.add("no")
+  
 }
 document.querySelectorAll('.no').forEach(el => {
   el.addEventListener('click',()=> {
